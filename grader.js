1073e6556f6f5f3a9675c71c60794accfcd25162
Ubuntu <tgoerner@gmail.com>
2013-07-28 07:19:03 +0000

    nonworking URL yet needs better coding in assertURLExists

--- grader.js
+++ grader.js
@@ -16,15 +16,35 @@ chmod a+x grader.js
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 var URLFILE_DEFAULT = "http://sheltered-lake-3072.herokuapp.com";
 
+var assertURLExists = function(url) {
+  rest.get(url).on('complete', function(results) {
+    if (results instanceof Error) {
+      console.log('Error getting url' + results.message);
+    } else {
+      fs.writeFile("/tmp/test.html", results, function(err, buffer) {
+        if (err instanceof Error) {
+           console.log("There was a write error" + err);
+        } else {
+          console.log("The file was saved!");
+        }
+      }); 
+    }
+  });
+//THIS PART DOES NOT WORK YET
+  var instr = fs.readFileSync("test.html").toString;
+  return instr; 
+};
+
 var assertFileExists = function(infile) {
   var instr = infile.toString();
-  if (!fs.existsSync(instr)) {
+  if(!fs.existsSync(instr)) {
     console.log("%s does not exist. Exiting.", instr);
-    process.exit(1);
+    process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
   }
   return instr;
 };
@@ -38,27 +58,27 @@ var loadChecks = function(checksfile) {
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-      var present = $(checks[ii]).length > 0;
-      out[checks[ii]] = present;
-    }
-    return out;
+  $ = cheerioHtmlFile(htmlfile);
+  var checks = loadChecks(checksfile).sort();
+  var out = {};
+  for(var ii in checks) {
+    var present = $(checks[ii]).length > 0;
+    out[checks[ii]] = present;
+  }
+  return out;
 };
 
 var clone = function(fn) {
-    // Workaround for commander.js issue.
-    // http://stackoverflow.com/a/6772648
-    return fn.bind({});
+  // Workaround for commander.js issue.
+  // http://stackoverflow.com/a/6772648
+  return fn.bind({});
 };
 
 if(require.main == module) {
     program
             .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
             .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-	    .option('-u, --url <url_file>', 'Path to url', clone(assertFileExists), URLFILE_DEFAULT)
+	    .option('-u, --url <url_file>', 'Path to url', clone(assertURLExists), URLFILE_DEFAULT)
             .parse(process.argv);
   var checkJson = checkHtmlFile(program.file, program.checks);
   var outJson = JSON.stringify(checkJson, null, 4);
